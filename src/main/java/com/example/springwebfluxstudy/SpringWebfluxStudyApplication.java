4e52848151ee2a70bfb23c6d310a83fe1b3e2a75
Minjun-KANG <inventer@kakao.com>
2023-06-16 08:39:28 +0900

    Initial commit

---
+++ src/main/java/com/example/springwebfluxstudy/SpringWebfluxStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxStudyApplication.class, args);
+    }
+
+}
